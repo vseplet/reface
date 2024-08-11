c9a373696c009a8c4d401785c863cc480055258b
Vsevolod Pletnev <vseplet@gmail.com>
2024-08-11 13:13:30 +0700

    init RefaceHono

---
+++ source/layouts/clean.ts
@@ -0,0 +1,31 @@
+// deno-lint-ignore-file no-unused-vars
+import { htmx } from "$/resources/scripts.ts";
+import { html, RefaceLayout } from "$/mod.ts";
+import type { BaseAppOptions } from "$types";
+
+/**
+ * TWA is a layout for building TWA (Telegram Web Apps)
+ * @param config - Layout configuration
+ * @returns Layout function
+ */
+export const clean = RefaceLayout<{
+  htmx?: boolean;
+}>((options) => {
+  return (page: string, appOptions: BaseAppOptions) => {
+    return html`
+      <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        ${options.htmx ? htmx : ""}
+        <script src="${appOptions.staticPath}/script.js"></script>
+        <link rel="stylesheet" href="${appOptions.staticPath}/style.css">
+        <title>${options.title || "Reface Clean"}</title>
+      </head>
+      <body>
+        ${page}
+      </body>
+    </html>
+    `;
+  };
+});
--- source/mod.ts
+++ source/mod.ts
@@ -1,7 +1,7 @@
 import { RefaceLayout } from "./entities/Layout.ts";
 import { Hono } from "@hono/hono";
-import { BaseAppOptions, Layout } from "$types";
-import { twa } from "$/layouts/twa.ts";
+import type { BaseAppOptions, Layout } from "$types";
+import { clean } from "$/layouts/clean.ts";
 
 const css = String.raw;
 const html = String.raw;
@@ -9,13 +9,13 @@ const js = String.raw;
 const salt = (name?: string) =>
   name ? `${name}_${crypto.randomUUID()}` : crypto.randomUUID();
 
-export const RefacePage = <T>(
+const RefacePage = <T>(
   _: {
     render: (props: { data: T; pageRoute: string }) => string;
   },
 ) => {};
 
-export const RefaceComponent = <T>(
+const RefaceComponent = <T>(
   _: {
     render: (
       props: { data: T; apiRoute: string; componentRoute: string },
@@ -24,31 +24,63 @@ export const RefaceComponent = <T>(
   },
 ) => {};
 
-export const RefaceElement = <T>(_: (props: { data: T }) => string) => {};
+const RefaceElement = <T>(_: (props: { data: T }) => string) => {};
 
-export class RefaceApp<T> {
+class RefaceHono<T> {
+  #router: Hono;
+  #defaultLayout: Layout;
+  #components: typeof RefaceComponent<T>[] = [];
+  #pages: {
+    route: string;
+    page: typeof RefacePage<T>;
+    layout: Layout;
+  }[] = [];
+
   constructor(
-    config: BaseAppOptions & {
+    config?: BaseAppOptions & {
       layout: Layout;
     },
   ) {
+    this.#router = new Hono();
+    this.#defaultLayout = config?.layout || clean({ htmx: true });
   }
 
-  getHonoRouter() {
-    return new Hono();
+  component(component: typeof RefaceComponent<T>) {
+    this.#components.push(component);
+    return this;
+  }
+
+  page(
+    route: string,
+    page: typeof RefacePage<T>,
+    options?: {
+      layout?: Layout;
+    },
+  ) {
+    this.#pages.push({
+      route,
+      page,
+      layout: options?.layout || this.#defaultLayout,
+    });
+    return this;
+  }
+
+  getRouter() {
+    return this.#router;
   }
 }
 
-// RefaceComponent<{ a: string; b: number }>({
-//   render: ({ data, componentRoute }) => {
-//     return html`
-//       <div>
-//         <h1>Hello ${data}</h1>
-//       </div>
-//     `;
-//   },
-// });
+export {
+  css,
+  html,
+  js,
+  RefaceComponent,
+  RefaceElement,
+  RefaceHono,
+  RefaceLayout,
+  RefacePage,
+  salt,
+};
 
-export { css, html, js, RefaceLayout, salt };
 export * from "@hono/hono";
 // Layout -> Page  -> Component -> Element
