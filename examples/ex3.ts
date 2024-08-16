c882c71b4b144f0985e2275f9db68e3d0a3665c5
Vsevolod Pletnev <vseplet@gmail.com>
2024-08-17 01:51:06 +0700

    upd ex3

--- examples/ex3.ts
+++ examples/ex3.ts
@@ -1,16 +1,51 @@
 import {
   component,
+  DELETE,
   GET,
   Hono,
   html,
   island,
   POST,
   Reface,
+  render,
   RESPONSE,
   twa,
 } from "jsr:@vseplet/reface@^0.0.16";
 
 const kv = await Deno.openKv();
+
+const Row = island<
+  {
+    index: number;
+    key: string[];
+    value: any;
+    versionstamp: string;
+  }
+>((props) => {
+  // deno-fmt-ignore
+  return html`
+    <tr class="align-middle">
+      <th scope="row">${props.index}</th>
+      <td>${props.key.join(" ")}</td>
+      <td>${JSON.stringify(props.value)}</td>
+      <td>${props.versionstamp}</td>
+      <td>
+        <button
+          class="btn btn-danger"
+          hx-delete="${props.api}/delete/${props.key.join("|")}"
+          hx-target="#alerts">
+          Delete
+        </button>
+      </td>
+    </tr>
+  `
+}, {
+  [DELETE("/delete/:key")]: async (props) => {
+    const key = props.params.key.split("|");
+    await kv.delete(key);
+    return RESPONSE(Alert("warning", `Value by key: ${key.join(" ")} deleted`));
+  },
+});
 
 const ListOfEntries = island((props) => {
   return html`
@@ -29,29 +64,29 @@ const ListOfEntries = island((props) => {
 
   `;
 }, {
-  [GET("/entries")]: async () => {
+  [GET("/entries")]: async (props) => {
     const allEntries = await Array.fromAsync(kv.list({ prefix: [] }));
     const rows = allEntries.map((entry, index) => {
-      return `
-        <tr>
-          <th scope="row">${index}</th>
-          <td>${entry.key.join(" ")}</td>
-          <td>${JSON.stringify(entry.value)}</td>
-          <td>${entry.versionstamp}</td>
-        </tr>
-      `;
+      return render(
+        Row({
+          index: index + 1,
+          key: entry.key as string[],
+          value: entry.value,
+          versionstamp: entry.versionstamp,
+        }),
+      );
     }).join("");
     return RESPONSE(rows);
   },
 });
 
-const Alert = (type: "success" | "danger", message: string) => {
+const Alert = (type: "success" | "danger" | "warning", message: string) => {
   return html`<div class="alert alert-${type}" role="alert" _="on load wait 4s then remove me">${message}</div>`;
 };
 
 const SubmitValue = island((props) => {
   return html`
-    <form hx-post="${props.api}/submit" hx-target="#result" class="form-group">
+    <form hx-post="${props.api}/submit" hx-target="#alerts" class="form-group">
       <div class="row mb-3">
         <div class="col">
           <label for="key" class="form-label">KEY</label>
@@ -69,7 +104,6 @@ const SubmitValue = island((props) => {
           <button type="submit" class="btn btn-primary w-100">Submit</button>
         </div>
       </div>
-      <div id="result" class="mt-3"></div>
     </form>
   `;
 }, {
@@ -103,6 +137,8 @@ const Home = component(() => {
       <div class="mt-5">
         ${ListOfEntries({})}
       </div>
+      <hr class="my-5" />
+      <div id="alerts"></div>
     </div>
   `;
 });
